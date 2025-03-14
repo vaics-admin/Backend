24b54a37405cb50838d7d7001b9c96509708e435
vaics-admin <vaics.superuser@gmail.com>
2025-03-14 12:43:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 """ Tying it all together with 'main.py' """
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")  # Add a route for the home page
-def home():
-    return "Flask App is Running Successfully on Render!"
+from src.app import app
 
 if __name__ == "__main__":
     import os
     port = int(os.getenv("PORT", 5000))  # Use Render's PORT environment variable
     app.run(host="0.0.0.0", port=port, debug=True)
-
