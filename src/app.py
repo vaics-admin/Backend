7425c67e790a0218c1591720208183d9f7c082eb
vaics-admin <vaics.superuser@gmail.com>
2025-03-14 14:47:31 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure CORS with specific options
-CORS(app, resources={r"/*": {"origins": "https://backend-dnxz.onrender.com"}},
+CORS(app, resources={r"/*": {"origins": "http://10.220.85.4:10000"}},
      supports_credentials=True,
      allow_headers=["Content-Type", "Authorization"],
      methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"])
@@ -19,19 +19,19 @@ db = SQLAlchemy(app)
 app.config['SESSION_SQLALCHEMY'] = db
 
 # Register blueprints
-from src.login.route import login_bp
+from .login.route import login_bp
 app.register_blueprint(login_bp, url_prefix='/login')
 
-from src.leave_management.routes import leave_management_bp
+from .leave_management.routes import leave_management_bp
 app.register_blueprint(leave_management_bp, url_prefix='/leave_management')
 
-from src.pms_employee.routes import pms_employee_bp
+from .pms_employee.routes import pms_employee_bp
 app.register_blueprint(pms_employee_bp, url_prefix='/pms_employee')
 
-from src.employee_profile.routes import employee_profile_bp
+from .employee_profile import employee_profile_bp
 app.register_blueprint(employee_profile_bp, url_prefix='/employee')
 
-from src.quick_links.passwordchange.routes import passwordchange_bp
+from .quick_links.passwordchange.routes import passwordchange_bp
 app.register_blueprint(passwordchange_bp, url_prefix='/quicklinks/passwordchange')
 
 # Initialize database
@@ -42,6 +42,10 @@ with app.app_context():
 @app.route("/")
 def home():
     return "Flask App is Running Successfully on Render!"
+
+@app.route("/login")
+def login():
+    return "login"
 
 # Run the app
 if __name__ == "__main__":
