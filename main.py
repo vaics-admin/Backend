105fb41f29b3f1bd9964fb4f9ebec1adcabd8dd6
vaics-admin <vaics.superuser@gmail.com>
2025-03-14 11:53:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """ Tying it all together with 'main.py' """
 
+import os
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Use Render's PORT environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)
+
